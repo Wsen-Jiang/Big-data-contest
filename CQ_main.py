9eedd7c060c9b565e8b45a3e059fb0305591f55d
WenSen Jiang <519712523@qq.com>
2024-10-22 19:33:51 +0800

    修改CQ_main的参数train_mode为task，与main函数对应

--- CQ_main.py
+++ CQ_main.py
@@ -13,22 +13,22 @@ from loss.CQ_CosineSimilarity import CosineSimilarityLoss
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from models.CQ_Seq2Seq import Encoder, Decoder
 
-def train(model, train_loader, val_loader, criterion, optimizer, device, num_epochs, train_mode):
+def train(model, train_loader, val_loader, criterion, optimizer, device, num_epochs, task):
 
     # 设置最佳指标：码元宽度回归任务以最小化损失为目标，调制类型分类任务以最大化准确率为目标，码序列相似度任务以最大化余弦相似度为目标
-    best_metric = float('inf') if train_mode == "SW" else 0
+    best_metric = float('inf') if task == "SW" else 0
 
     history_train_loss = []
     history_valid_loss = []
 
-    if train_mode == "MT":
+    if task == "MT":
         end_path = "ModulationType"
-    elif train_mode == "SW":
+    elif task == "SW":
         end_path = "SymbolWidth"
-    elif train_mode == "CQ":
+    elif task == "CQ":
         end_path = "CodeSequence"
     else:
-        raise ValueError(f"无效的 train_mode 参数: {train_mode}")
+        raise ValueError(f"无效的 task 参数: {task}")
     model_dir = f'log/models/{end_path}/{model.__class__.__name__}'
     os.makedirs(model_dir, exist_ok=True)
 
@@ -74,7 +74,7 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, num_epo
         model.eval()
         sum_val_loss = 0.0
         total_samples = 0  # 用于累加样本数量
-        if train_mode == "MT":
+        if task == "MT":
             correct = 0
             total = 0
         else:
@@ -94,22 +94,22 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, num_epo
                 # max_label_length = label_lengths.max().item()
                 # outputs = outputs[:, :max_label_length, :]  # 截取到最大标签长度
 
-                if train_mode == "SW":
+                if task == "SW":
                     loss = criterion(outputs, batch_y.view(-1, 1))
                     sum_val_loss += loss.item()  # 累加总损失
                     all_preds.extend(outputs.cpu().numpy())
                     all_targets.extend(batch_y.cpu().numpy())
                     total_samples += batch_X.size(0)  # 累加样本数
-                elif train_mode == "MT":
+                elif task == "MT":
                     loss = criterion(outputs, batch_y)
                     sum_val_loss += loss.item()
                     _, predicted = torch.max(outputs.data, 1)
                     total += batch_y.size(0)
                     correct += (predicted == batch_y).sum().item()
-                elif train_mode == "CQ":
+                elif task == "CQ":
                     # loss = criterion(outputs, labels_embedded,label_lengths)
                     sum_val_loss += loss.item()
-        if train_mode == "SW":
+        if task == "SW":
             avg_val_loss = sum_val_loss / total_samples  # 计算平均损失
             mse = mean_squared_error(all_targets, all_preds)
             rmse = mean_squared_error(all_targets, all_preds, squared=False)
@@ -121,7 +121,7 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, num_epo
                 best_metric = mse
                 # 保存最佳模型
                 torch.save(model.state_dict(), os.path.join(model_dir, f'best_model.pth'))
-        elif train_mode == "MT":
+        elif task == "MT":
             avg_val_loss = sum_val_loss / len(val_loader)  # 对于分类任务，可以保持原有方式
             accuracy = 100 * correct / total
             print(f'Epoch [{epoch + 1}/{num_epochs}], Val Loss: {avg_val_loss:.4f}, Accuracy: {accuracy:.2f}%')
@@ -129,7 +129,7 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, num_epo
                 best_metric = accuracy
                 # 保存最佳模型
                 torch.save(model.state_dict(), os.path.join(model_dir, f'best_model.pth'))
-        elif train_mode == "CQ":
+        elif task == "CQ":
             avg_val_loss = sum_val_loss / len(val_loader)
             print(f'Epoch [{epoch + 1}/{num_epochs}], Val Loss: {avg_val_loss:.4f}')
 
@@ -140,7 +140,7 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, num_epo
         history_valid_loss.append(avg_val_loss)
 
     # 保存最终模型
-    torch.save(model.state_dict(), os.path.join(model_dir,'final_model.pth'))
+    torch.save(model.state_dict(), os.path.join(model_dir,'0.2015_46.95_final_model.pth'))
 
     # loss图保存路径
     save_path = f'log/save_loss/{end_path}/{model.__class__.__name__}'
@@ -149,7 +149,7 @@ def train(model, train_loader, val_loader, criterion, optimizer, device, num_epo
               f"{save_path}/{model.__class__.__name__}_{args.lr}_{args.batch_size}_{best_metric}.png")
 
 
-def test(model, val_loader, criterion, device, model_path, train_mode):
+def test(model, val_loader, criterion, device, model_path, task):
     # 加载已保存的模型权重
     if os.path.exists(model_path):
         model.load_state_dict(torch.load(model_path, map_location=device))
@@ -162,7 +162,7 @@ def test(model, val_loader, criterion, device, model_path, train_mode):
 
     val_loss = 0.0
     total_samples = 0
-    if train_mode in ["MT", "CQ"]:
+    if task in ["MT", "CQ"]:
         correct = 0
         total = 0
     else:
@@ -178,7 +178,7 @@ def test(model, val_loader, criterion, device, model_path, train_mode):
 
             outputs = model(batch_X)
 
-            if train_mode == "SW":
+            if task == "SW":
                 loss = criterion(outputs, batch_y.view(-1, 1))
                 val_loss += loss.item()  # 累加总损失
                 all_preds.extend(outputs.cpu().numpy())
@@ -191,11 +191,11 @@ def test(model, val_loader, criterion, device, model_path, train_mode):
                 total += batch_y.size(0)
                 correct += (predicted == batch_y).sum().item()
 
-    if train_mode in ["MT", "CQ"]:
+    if task in ["MT", "CQ"]:
         avg_val_loss = val_loss / len(val_loader)
         accuracy = 100 * correct / total
         print(f'验证集上的损失: {avg_val_loss:.4f}, 准确率: {accuracy:.2f}%')
-    elif train_mode == "SW":
+    elif task == "SW":
         avg_val_loss = val_loss / total_samples
         mse = mean_squared_error(all_targets, all_preds)
         rmse = mean_squared_error(all_targets, all_preds, squared=False)
@@ -206,7 +206,7 @@ def test(model, val_loader, criterion, device, model_path, train_mode):
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser("Train or test the model")
     arg_parser.add_argument("--mode", type=str, default="train", help="train or test")
-    arg_parser.add_argument("--train_mode", type=str, default="CQ",
+    arg_parser.add_argument("--task", type=str, default="CQ",
                             help="MT(ModulationType)、SW(SymbolWidth)、CQ(CodeSequence)")
     arg_parser.add_argument("--network", type=str, default="CQ_Seq2Seq",
                             help="选择网络 (例如 CNNClassifier, ResNet)")
@@ -223,10 +223,10 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # 读取数据
-    sequences, labels = load_data_from_directories(root_dir, data_dirs, args.train_mode)
+    sequences, labels = load_data_from_directories(root_dir, data_dirs, args.task)
 
     # 划分训练集和验证集
-    if args.train_mode == "MT":
+    if args.task == "MT":
         stratify = labels
     else:
         stratify = None
@@ -234,7 +234,7 @@ if __name__ == "__main__":
     seq_train, seq_val, y_train, y_val = train_test_split(
         sequences, labels, test_size=0.2, random_state=42, stratify=stratify
     )
-    collate_fn = CollateFunction(args.train_mode)
+    collate_fn = CollateFunction(args.task)
     # 创建数据集和数据加载器
     if args.mode == 'train':
         train_dataset = WaveformDataset(seq_train, y_train)
@@ -269,14 +269,14 @@ if __name__ == "__main__":
         raise ValueError(f"指定的网络 {args.network} 无效或不存在")
 
     # 定义损失函数和优化器
-    if args.train_mode == "MT":
+    if args.task == "MT":
         criterion = nn.CrossEntropyLoss()
-    elif args.train_mode == "SW":
+    elif args.task == "SW":
         criterion = RelativeErrorLoss()
-    elif args.train_mode == "CQ":
+    elif args.task == "CQ":
         criterion = CosineSimilarityLoss(model.decoder)
     else:
-        raise ValueError(f"无效的 train_mode 参数: {args.train_mode}")
+        raise ValueError(f"无效的 task 参数: {args.task}")
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
@@ -284,9 +284,9 @@ if __name__ == "__main__":
 
     if args.mode == 'train':
         # 训练模型
-        train(model, train_loader, val_loader, criterion, optimizer, device, args.epochs, args.train_mode)
+        train(model, train_loader, val_loader, criterion, optimizer, device, args.epochs, args.task)
     elif args.mode == 'test':
         if not args.model_path:
             print("测试模式需要指定模型文件路径，请使用 --model_path 参数。")
             exit(1)
-        test(model, val_loader, criterion, device, args.model_path, args.train_mode)
+        test(model, val_loader, criterion, device, args.model_path, args.task)

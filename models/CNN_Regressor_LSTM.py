2664110c9c60227334f804fac0a65ab76becd227
LIW-1314 <58169371+LIW-1314@users.noreply.github.com>
2024-11-28 20:51:56 +0800

    Add files via upload

--- models/CNN_Regressor_LSTM.py
+++ models/CNN_Regressor_LSTM.py
@@ -1,13 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-class CNN_LSTM_Class(nn.Module):
-    def __init__(self, channels=2, num_classes=10, dropout=0.5):
-        super(CNN_LSTM_Class, self).__init__()
+class CNN_Regressor_LSTM(nn.Module):
+    def __init__(self, channels=2, out_dim=1, dropout=0.5):
+        super(CNN_Regressor_LSTM, self).__init__()
         # (Same model as before)
         self.conv1 = nn.Conv1d(channels, 64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm1d(64)
-        self.conv2 = nn.Conv1d(64, 128, kernel_size=7, stride=1, padding=3)
+        self.conv2 = nn.Conv1d(64, 128, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm1d(128)
         self.conv3 = nn.Conv1d(128, 256, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm1d(256)
@@ -15,7 +15,7 @@ class CNN_LSTM_Class(nn.Module):
 
         self.lstm = nn.LSTM(input_size=256, hidden_size=128, num_layers=2, batch_first=True, bidirectional=True)
         self.dropout = nn.Dropout(dropout)
-        self.fc1 = nn.Linear(256, num_classes)
+        self.fc1 = nn.Linear(256, out_dim)
 
         # 参数初始化
         nn.init.kaiming_uniform_(self.conv1.weight, mode='fan_in', nonlinearity='relu')

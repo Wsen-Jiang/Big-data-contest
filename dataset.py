fe65a595fc9ef45d34d462b972c94a735000bc64
lixiangpengcs <lixiangpengcs@163.com>
2024-11-30 20:31:55 +0800

    增加数据处理之后的缓存以提升后续效率

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import os
 import torch
 import numpy as np
 import pandas as pd
+import pickle as pkl
 from torch.nn.utils.rnn import pad_sequence
 # 自定义collate_fn类，用于处理变长序列
 # 在处理变长序列时，默认的 DataLoader 无法将不同长度的序列组合成一个批次，因此需要自定义 collate_fn 函数来实现填充操作。
@@ -60,45 +61,58 @@ def normalization(input_tensor):
 
 # 数据读取函数
 def load_data_from_directories(root_dir, data_dirs,train_mode):
-    sequences = []
-    labels = []
+    if os.path.exists(os.path.join(root_dir, 'cache', 'processed_data.pkl')):
+        with open(os.path.join(root_dir, 'cache', 'processed_data.pkl'), 'rb') as f:
+            processed_data = pkl.load(f)
+            sequences = processed_data['sequences']
+            labels = processed_data['labels']
+    else:
+        sequences = []
+        labels = []
 
-    # 遍历每个目录
-    for dir_name in data_dirs:
-        dir_path = os.path.join(root_dir, dir_name)
-        for file_name in os.listdir(dir_path):
-            if file_name.endswith('.csv'):
-                file_path = os.path.join(dir_path, file_name)
-                try:
-                    # 读取CSV文件，无标题行
-                    data = pd.read_excel(file_path, engine='xlrd', header=None)
+        # 遍历每个目录
+        for dir_name in data_dirs:
+            dir_path = os.path.join(root_dir, dir_name)
+            for file_name in os.listdir(dir_path):
+                if file_name.endswith('.csv'):
+                    file_path = os.path.join(dir_path, file_name)
+                    try:
+                        # 读取CSV文件，无标题行
+                        # data = pd.read_excel(file_path, engine='xlrd', header=None)
+                        data = pd.read_csv(file_path, header=None)
 
-                    # 尝试提取第一列和第二列作为输入特征
-                    sequence = data.iloc[:, [0, 1]].values.astype(np.float32)
-                    sequence = torch.tensor(sequence)
-                    # 添加到列表
-                    sequences.append(sequence)
+                        # 尝试提取第一列和第二列作为输入特征
+                        sequence = data.iloc[:, [0, 1]].values.astype(np.float32)
+                        sequence = torch.tensor(sequence)
+                        # 添加到列表
+                        sequences.append(sequence)
 
-                    # 调制类型
-                    ModulationType = int(data.iloc[0, 3]) - 1 # 获取第四列的第一个元素，0 表示第一行，3 表示第四列
-                    # 码元宽度
-                    SymbolWidth = round(float(data.iloc[0, 4]), 2)
-                    # 码序列
-                    data = data.dropna(subset=[2]) # 删除码序列中的 NaN 值
-                    CodeSequence = data.iloc[:,2].values.astype(np.int32)
+                        # 调制类型
+                        ModulationType = int(data.iloc[0, 3]) - 1 # 获取第四列的第一个元素，0 表示第一行，3 表示第四列
+                        # 码元宽度
+                        SymbolWidth = round(float(data.iloc[0, 4]), 2)
+                        # 码序列
+                        data = data.dropna(subset=[2]) # 删除码序列中的 NaN 值
+                        CodeSequence = data.iloc[:,2].values.astype(np.int32)
 
-                    if train_mode == "MT":
-                        label = ModulationType
-                    elif train_mode == "SW":
-                        label = SymbolWidth
-                    elif train_mode == "CQ":
-                        label = CodeSequence
-                    else:
-                        raise ValueError(f"无效的 train_model 参数: {train_mode}")
-                    label = torch.tensor(label)
-                    labels.append(label)
+                        if train_mode == "MT":
+                            label = ModulationType
+                        elif train_mode == "SW":
+                            label = SymbolWidth
+                        elif train_mode == "CQ":
+                            label = CodeSequence
+                        else:
+                            raise ValueError(f"无效的 train_model 参数: {train_mode}")
+                        label = torch.tensor(label)
+                        labels.append(label)
 
-                except IndexError:
-                    print(f"文件 {file_name} 的列数不足，跳过该文件")
+                    except IndexError:
+                        print(f"文件 {file_name} 的列数不足，跳过该文件")
 
+        os.mkdir(os.path.join(root_dir, 'cache'))
+        with open(os.path.join(root_dir, 'cache', 'processed_data.pkl'), 'wb') as f:
+            processed_data = {}
+            processed_data['sequences'] = sequences
+            processed_data['labels'] = labels
+            pkl.dump(processed_data, f)
     return sequences, labels
